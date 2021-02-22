33ae060fe6bb7f76d3dd73e2e20e40bd6d76a719
waynedmustt <muhammaddimas04@gmail.com>
2021-02-22 21:08:47 +0800

    add db image

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,15 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BullModule } from '@nestjs/bull';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [
     BullModule.forRoot({
       redis: {
-        host: 'app_redis',
+        host: 'redis',
         port: 6379
       }
-    })
+    }),
+    TypeOrmModule.forRoot()
   ],
   controllers: [AppController],
   providers: [AppService],
